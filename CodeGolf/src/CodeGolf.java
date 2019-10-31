bb4d4e282d0feb894fb9ed030d70325814c7b1e1
Caio Massucato <caiomassucato@localhost.localdomain>
2019-10-31 15:06:02 -0300

    Prontin

--- CodeGolf/src/CodeGolf.java
+++ CodeGolf/src/CodeGolf.java
@@ -1,38 +1,47 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class CodeGolf {
 
 	public static void main(String[] args) throws IOException {
 		
-		// Inputs
+		// Inputs Declarations
 		FileInputStream fIn = new FileInputStream("words.txt");
 		Scanner fscan = new Scanner(fIn);
-		// Array Lists
+		FileInputStream stpIn = new FileInputStream("stopwords.txt");
+		Scanner stpscan = new Scanner(stpIn);
+		
+		// Array Lists Declarations
 		ArrayList<String> words = new ArrayList<String>();
 		ArrayList<Integer> count = new ArrayList<Integer>();
-		
+		ArrayList<String> stpwords = new ArrayList<String>();
+
+		// Stop Words List
+		while (stpscan.hasNext()) {
+			stpwords.add(stpscan.nextLine());}
 		// Reading file and comparing words
 		while(fscan.hasNext()) {
 			// Reach next word
 			String nextWord = fscan.next();
+			nextWord = nextWord.replaceAll("\\p{Punct}", "").toLowerCase();
 			// Check if it's in the word list
-			if (words.contains(nextWord)) {
+			if (words.contains(nextWord.toLowerCase())) {
 				int index = words.indexOf(nextWord);
-				count.set(index, count.get(index) + 1);
-			}
+				count.set(index, count.get(index) + 1);}
 			else {
-				words.add(nextWord); count.add(1);	
-			}
-		}
-		// Close files
-		fscan.close(); fIn.close();
+				if(!stpwords.contains(nextWord.toLowerCase())) {
+					words.add(nextWord.toLowerCase()); count.add(1);}}}		
+		count.remove(Collections.max(count));
 		// Output
-		for(int i = 0; i < words.size(); i++) {
-			System.out.println(words.get(i) + " : " + count.get(i));
-		}
+		int maxindex = count.indexOf(Collections.max(count));
+		System.out.println(words.get(maxindex));
+
+		
+		// Close files
+		fscan.close(); fIn.close(); stpIn.close(); stpscan.close();
 	}
 
 }
